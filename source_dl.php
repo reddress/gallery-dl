6f676d255353872344328a6f98e992fa6c718874
Heitor Chang <heitorchang@gmail.com>
2016-04-09 21:16:57 -0300

    dl from source

---
+++ source_dl.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Source DL Get</title>
+    <body>
+      
+      <?php
+      // echo $_POST["links"];
+
+      $raw_links = trim($_POST["links"]);
+      // fake user agent
+      // https://joshtronic.com/2013/06/04/specifying-a-user-agent-when-using-file_get_contents/
+
+      $options  = array('http' => array('user_agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:45.0) Gecko/20100101 Firefox/45.0'));
+      $context  = stream_context_create($options);
+      $imagetype = ".jpg";
+
+      $dest_folder = $_POST['saveas'] . $_POST['newfolder'];
+
+      if (!file_exists($dest_folder)) {
+        mkdir($dest_folder, 0777, true);
+      }
+      
+      $cur_num = 1;
+
+      foreach(explode("\n", $raw_links) as $raw_link) {
+
+        $save_to_filename = str_pad($cur_num, 3, "0", STR_PAD_LEFT) . $imagetype;
+          
+        $source = $raw_link;
+
+        // $dest = $dest_folder . "/" . $filename;
+        $dest = $dest_folder . "/" . $save_to_filename;
+
+        echo("file_put_contents(\"$dest\", file_get_contents(\"$source\")");
+        echo("<br>");
+        echo("<br>");
+        flush();
+        ob_flush();
+        
+        $cur_num++;
+        
+        file_put_contents($dest, file_get_contents($source, false, $context));
+      }
+      
+      ?>
+      <a href="display.php?basedir=<?= $_POST['newfolder'] ?>">View Gallery</a>
+    </body>
+</html>
---
+++ source_dl_def.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Source DL</title>
+  </head>
+  <body>
+    Gallery Download (from Source) v.0.1 2016-04-09<br>
+    <a href="display.php" target="_blank">Display galleries</a>
+    <hr>
+
+    <form action="source_dl.php" method="POST" target="_blank">
+      <input type="button" onclick="resetform();" value="Reset form">
+      <br><br>
+
+      Base URL (include http://) <input type="text" id="baseurl"><br>
+      <textarea id="raw_source" rows="10" cols="60"></textarea>
+      <br>
+      <button type="button" id="extract">Extract links</button>
+      Total Links: 
+      <span id="totalLinks"></span>
+      <br>
+      <textarea id="links" name="links" rows="8" cols="100"></textarea>
+      <br>
+
+      Local Prefix to Save as: <input name="saveas" value="/home/heitor/saved_galleries/">
+      <br>
+      New local folder: <input name="newfolder" value="<?= date('YmdHis') ?>">
+      <input type="submit">
+
+    </form>
+    <script src="jquery.js"></script>
+    <script>
+     var link_matches;
+     var totalLinks = 0;
+     $("#extract").click(function() {
+       $("#links").val("");
+       var baseurl = $("#baseurl").val();
+       link_matches = $($("#raw_source").val()).find("a");
+       $.each(link_matches, function(key, value) {
+         var link_href = $(value).attr("href");
+         var extension = link_href.substr(link_href.length - 3);
+         console.log(link_href + " " + extension);
+         if (extension === "jpg" || extension === "JPG") {
+           $("#links").val($("#links").val() + baseurl + link_href + "\n");
+           totalLinks++;
+         }
+       });
+       $("#totalLinks").html(totalLinks);
+     });
+
+     function timestamp() {
+       var now = new Date();
+       var nowLocal = new Date(now.getTime() - (now.getTimezoneOffset() * 60000));
+       var nowStr = nowLocal.toISOString();
+       //return "" + now.getFullYear() + "_" + (1 + now.getMonth()) + "_" + now.getDate() + "_" + now.getHours() + "_" + now.getMinutes() + "_" + now.getSeconds();
+       return nowStr.slice(0, 10).replace(/-/g, '') + nowStr.slice(11, 19).replace(/:/g, "");
+     }
+     
+     function resetform() {
+       console.log("resetting");
+       document.forms[0].elements['baseurl'].value = "";
+       $("#raw_source").val("");
+       $("#links").val("");
+       document.forms[0].elements['newfolder'].value = timestamp();
+       $("#totalLinks").html("");
+     }
+
+    </script>
+  </body>
+</html>
