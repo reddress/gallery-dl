6c54fbdaeffd6ef6cd926bc96da5c3e20b49b16e
Heitor Chang <heitorchang@gmail.com>
2016-05-14 11:05:12 -0300

    reorder index

--- index.php
+++ index.php
@@ -1,80 +1,76 @@
-Gallery Download v.0.4 2016-05-14
-<hr>
-<a href="source_dl_def.php">Download from source</a>
-<hr>
-
-<a href="thumbs.php" target="_blank">Display galleries</a>
-<hr>
-
-<form action="dl.php" method="post" target="_blank">
-    Download a gallery of images, such as<br>
-    http://site.com/img/0.jpg<br>
-    http://site.com/img/1.jpg<br>
-    http://site.com/img/2.jpg ...
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Source DL</title>
+  </head>
+  <body>
+    Gallery Download (from Source) v.0.1.1 2016-05-08<hr>
+    <a href="thumbs.php" target="_blank">Display galleries</a>
     <hr>
-    <input type="button" onclick="resetform();" value="Reset form">
 
-    <table>
-        <tr>
-            <td>URL e.g http://site.com/img/0.jpg</td><td><input name="url" size="72"></td>
-        </tr>
-        <tr>
-            <td>Image name prefix (leave blank if numbers only)</td><td><input name="prefix" autocomplete="off"></td>
-        </tr>
-        <tr>
-            <td>Leading zeros (0 if first file is 0.jpg or 1.jpg)</td><td> <input name="leading" value="0" autocomplete="off"></td>
-        </tr>
-        <tr>
-            <td>Beginning number</td><td><input name="begin" autocomplete="off"></td>
-        </tr>
-        
-        <tr>
-            <td>Ending number</td><td><input name="end" autocomplete="off"></td>
-        </tr>
+    <a href="seq_dl.php">Sequential Download</a>
+    <hr>
 
-        <tr>
-            <td>Image name postfix</td><td> <input name="postfix" autocomplete="off"></td>
-        </tr>
+    <form action="source_dl.php" method="POST" target="_blank">
+      <input type="button" onclick="resetform();" value="Reset form">
+      <br><br>
 
-        <tr>
-            <td>Image type</td><td> <input name="imagetype" value=".jpg" autocomplete="off"></td>
-        </tr>
-        
-        <tr>
-            <td>Local Prefix to Save as</td><td> <input name="saveas" value="/home/heitor/saved_galleries/"></td>
-        </tr>
-        <tr>
-            <td>New local folder:</td><td><input name="newfolder" value="<?= date('YmdHis') ?>"></td>
-        </tr>
-        <tr>
-            <td>
-                <input type="submit">
-            </td>
-            <td>
-                WAIT after clicking submit
-            </td>
-        </tr>
-    </table>
-</form>
+      Base URL (include http://) <input type="text" id="baseurl"><br>
+      Source code (must include enclosing element, such as div): <br>
+      <textarea id="raw_source" rows="10" cols="60"></textarea>
+      <br>
+      <button type="button" id="extract">Extract links</button>
+      Total Links: 
+      <span id="totalLinks"></span>
+      <br>
+      <textarea id="links" name="links" rows="8" cols="100"></textarea>
+      <br>
 
+      Local Prefix to Save as: <input name="saveas" value="/home/heitor/saved_galleries/">
+      <br>
+      New local folder: <input name="newfolder" value="<?= date('YmdHis') ?>">
+      <input type="submit">
 
-<script>
- function timestamp() {
-   var now = new Date();
-   var nowLocal = new Date(now.getTime() - (now.getTimezoneOffset() * 60000));
-   var nowStr = nowLocal.toISOString();
-   //return "" + now.getFullYear() + "_" + (1 + now.getMonth()) + "_" + now.getDate() + "_" + now.getHours() + "_" + now.getMinutes() + "_" + now.getSeconds();
-   return nowStr.slice(0, 10).replace(/-/g, '') + nowStr.slice(11, 19).replace(/:/g, "");
- }
- 
- function resetform() {
-     console.log("resetting");
-     document.forms[0].elements['url'].value = "";
-     document.forms[0].elements['prefix'].value = "";
-     document.forms[0].elements['postfix'].value = "";
-     document.forms[0].elements['leading'].value = "0";
-     document.forms[0].elements['begin'].value = "";
-     document.forms[0].elements['end'].value = "";
-     document.forms[0].elements['newfolder'].value = timestamp();
- }
-</script>
+    </form>
+    <script src="jquery.js"></script>
+    <script>
+     var link_matches;
+     $("#extract").click(function() {
+       var totalLinks = 0;
+       $("#links").val("");
+       var baseurl = $("#baseurl").val();
+       link_matches = $($("#raw_source").val()).find("a");
+       $.each(link_matches, function(key, value) {
+         var link_href = $(value).attr("href");
+         var extension = link_href.substr(link_href.length - 3);
+         console.log(link_href + " " + extension);
+         if (extension === "jpg" || extension === "JPG") {
+           $("#links").val($("#links").val() + baseurl + link_href + "\n");
+           totalLinks++;
+         }
+       });
+       $("#totalLinks").html(totalLinks);
+     });
+
+     function timestamp() {
+       var now = new Date();
+       var nowLocal = new Date(now.getTime() - (now.getTimezoneOffset() * 60000));
+       var nowStr = nowLocal.toISOString();
+       //return "" + now.getFullYear() + "_" + (1 + now.getMonth()) + "_" + now.getDate() + "_" + now.getHours() + "_" + now.getMinutes() + "_" + now.getSeconds();
+       return nowStr.slice(0, 10).replace(/-/g, '') + nowStr.slice(11, 19).replace(/:/g, "");
+     }
+     
+     function resetform() {
+       console.log("resetting");
+       document.forms[0].elements['baseurl'].value = "";
+       $("#raw_source").val("");
+       $("#links").val("");
+       document.forms[0].elements['newfolder'].value = timestamp();
+       $("#totalLinks").html("");
+       totalLinks = 0;
+     }
+
+    </script>
+  </body>
+</html>
---
+++ seq_dl.php
@@ -0,0 +1,80 @@
+Gallery Download v.0.4 2016-05-14
+<hr>
+
+<a href="thumbs.php" target="_blank">Display galleries</a>
+<hr>
+<a href="index.php">Download from source</a>
+<hr>
+
+<form action="dl.php" method="post" target="_blank">
+    Download a gallery of images, such as<br>
+    http://site.com/img/0.jpg<br>
+    http://site.com/img/1.jpg<br>
+    http://site.com/img/2.jpg ...
+    <hr>
+    <input type="button" onclick="resetform();" value="Reset form">
+
+    <table>
+        <tr>
+            <td>URL e.g http://site.com/img/0.jpg</td><td><input name="url" size="72"></td>
+        </tr>
+        <tr>
+            <td>Image name prefix (leave blank if numbers only)</td><td><input name="prefix" autocomplete="off"></td>
+        </tr>
+        <tr>
+            <td>Leading zeros (0 if first file is 0.jpg or 1.jpg)</td><td> <input name="leading" value="0" autocomplete="off"></td>
+        </tr>
+        <tr>
+            <td>Beginning number</td><td><input name="begin" autocomplete="off"></td>
+        </tr>
+        
+        <tr>
+            <td>Ending number</td><td><input name="end" autocomplete="off"></td>
+        </tr>
+
+        <tr>
+            <td>Image name postfix</td><td> <input name="postfix" autocomplete="off"></td>
+        </tr>
+
+        <tr>
+            <td>Image type</td><td> <input name="imagetype" value=".jpg" autocomplete="off"></td>
+        </tr>
+        
+        <tr>
+            <td>Local Prefix to Save as</td><td> <input name="saveas" value="/home/heitor/saved_galleries/"></td>
+        </tr>
+        <tr>
+            <td>New local folder:</td><td><input name="newfolder" value="<?= date('YmdHis') ?>"></td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit">
+            </td>
+            <td>
+                WAIT after clicking submit
+            </td>
+        </tr>
+    </table>
+</form>
+
+
+<script>
+ function timestamp() {
+   var now = new Date();
+   var nowLocal = new Date(now.getTime() - (now.getTimezoneOffset() * 60000));
+   var nowStr = nowLocal.toISOString();
+   //return "" + now.getFullYear() + "_" + (1 + now.getMonth()) + "_" + now.getDate() + "_" + now.getHours() + "_" + now.getMinutes() + "_" + now.getSeconds();
+   return nowStr.slice(0, 10).replace(/-/g, '') + nowStr.slice(11, 19).replace(/:/g, "");
+ }
+ 
+ function resetform() {
+     console.log("resetting");
+     document.forms[0].elements['url'].value = "";
+     document.forms[0].elements['prefix'].value = "";
+     document.forms[0].elements['postfix'].value = "";
+     document.forms[0].elements['leading'].value = "0";
+     document.forms[0].elements['begin'].value = "";
+     document.forms[0].elements['end'].value = "";
+     document.forms[0].elements['newfolder'].value = timestamp();
+ }
+</script>
